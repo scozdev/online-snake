cb393a6fa381ef0c0fd203cc0f2897d3ca621ddc
Selçuk Özdemir <49730100+scozdev@users.noreply.github.com>
2020-09-22 16:14:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Twitter Clone Frontend
+ - Live: snake-game-scozdev.netlify.app/
+
+## :rocket: Tech Stack
+
+- NodeJs
+- Express
+- Socket
+
+## :warning: Prerequisite
+
+- node
+- npm
+
+## :cd: How to run local
+
+<code>npm i</code> and <code>npm start</code> 
+
+![Screen Recording](screenshots/video-snake.gif)
---
+++ server.js
@@ -0,0 +1,109 @@
+require('dotenv').config()
+if (process.env.server) {
+    var io = require('socket.io')();
+    io.listen(process.env.PORT || 3000);
+} else {
+    var app = require('express')();
+    var http = require('http').createServer(app);
+    var io = require('socket.io')(http);
+
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/frontend/index.html');
+    })
+
+    http.listen(3000, () => {
+        console.log('listeinng on *:3000')
+    })
+}
+
+
+var colors = ['red', 'blue', 'yellow', 'green', 'pink', 'aqua', 'brown', 'chocolate', 'gold', 'orange'];
+
+const players = {};
+const apple = { x: 15, y: 15 }
+let colorSize = 0;
+
+io.on("connection", (socket) => {
+    console.log(`a user connected ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`user disconnected ${socket.id}`);
+        delete players[socket.id];
+        clearInterval(gameLoop)
+    });
+
+    var player = null;
+
+    const tileCount = 30;
+
+
+    // new player
+
+    players[socket.id] = {
+        velocityX: 0,
+        velocityY: 0,
+        x: Math.floor(Math.random() * 10),
+        y: Math.floor(Math.random() * 10),
+        trail: [],
+        tailSize: 2,
+        color: colors[colorSize++],
+        playerId: socket.id
+    }
+    if (colorSize >= 9) colorSize = 0;
+
+    socket.on("key", (key) => {
+        player = players[socket.id];
+        if (key === 87 && player.velocityY != 1) {
+            player.velocityY = -1;
+            player.velocityX = 0;
+        } else if (key === 68 && player.velocityX != -1) {
+            player.velocityX = 1;
+            player.velocityY = 0;
+        } else if (key === 83 && player.velocityY != -1) {
+            player.velocityY = 1;
+            player.velocityX = 0;
+        } else if (key === 65 && player.velocityX != 1) {
+            player.velocityX = -1;
+            player.velocityY = 0;
+        }
+    });
+
+    function update() {
+        player = players[socket.id];
+
+        player.x += player.velocityX;
+        player.y += player.velocityY;
+
+        if (player.x > tileCount - 1) {
+            player.x = 0;
+        }
+        if (player.x < 0) {
+            player.x = tileCount - 1;
+        }
+        if (player.y > tileCount - 1) {
+            player.y = 0;
+        }
+        if (player.y < 0) {
+            player.y = tileCount - 1;
+        }
+
+        player.trail.push({ x: player.x, y: player.y });
+
+        while (player.trail.length > player.tailSize) {
+            player.trail.shift();
+        }
+
+        if (apple.x === player.x && apple.y == player.y) {
+            player.tailSize++;
+            apple.x = Math.floor(Math.random() * tileCount);
+            apple.y = Math.floor(Math.random() * tileCount);
+        }
+    }
+
+    var gameLoop = setInterval(() => {
+        update()
+        io.emit("state", players, apple);
+    }, 1000 / 15);
+
+});
+
